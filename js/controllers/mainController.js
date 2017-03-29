b23008e7a96e230054aa9aef786f34da97c0b656
Piotr-V <pierrevicq@hotmail.fr>
2017-03-29 21:10:09 +0200

    Done

--- js/controllers/mainController.js
+++ js/controllers/mainController.js
@@ -1,4 +1,20 @@
-// MAIN CONTROLLER
-function mainController($scope) {
-    
-}
+'use strict';
+
+angular.module('app', [])
+    .controller('mainController', function ($scope) {
+
+        $scope.personne = [];
+
+        $scope.addPersonne = function () {
+            var newPersonne = {
+                prenom: $scope.newPersonne.prenom,
+                nom: $scope.newPersonne.nom,
+                email: $scope.newPersonne.email
+            };
+            $scope.newPersonne.prenom = '';
+            $scope.newPersonne.nom = '';
+            $scope.newPersonne.email = '';
+            $scope.personne.push(newPersonne);
+        };
+
+    });
